f6d7dd18a56af195df4fd80d8b6461c79b64f395
Vincent <3d314831994@gmail.com>
2019-11-16 14:07:42 +1100

    Finished informationPanel and Setting Panel in game

---
+++ Assets/Scripts/VI/InGameUIController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUIController : MonoBehaviour
+{
+    private static InGameUIController _i;
+    public static InGameUIController i
+    {
+        get
+        {
+            if (_i == null)
+            {
+                _i = FindObjectOfType<InGameUIController>();
+            }
+            return _i;
+        }
+    }
+
+    [HideInInspector]
+    public List<Image> informationButtons = new List<Image>();
+    public List<GameObject> informationContents = new List<GameObject>();
+    GameObject tempContent;
+
+    public void InformationButtonSelectd(Image image)
+    {
+        if (image.enabled)
+        {
+            for (int i = 0; i < informationContents.Count; i++)
+            {
+                if (image.gameObject.name == informationContents[i].name) {
+                    informationContents[i].SetActive(true);
+                    tempContent = informationContents[i];
+                }
+            }
+            image.enabled = false;
+            informationContents.Remove(tempContent);
+            informationContents.Insert(0,tempContent);
+
+            informationButtons.Remove(image);
+            informationButtons.Insert(0, image);
+        }
+        else {
+            return;
+        }
+        //Close others
+        for (int i = 1; i < informationButtons.Count; i++)
+        {
+            informationContents[i].SetActive(false);
+            informationButtons[i].enabled = true;
+        }
+    }
+}
--- Assets/Scripts/VI/UI_Button.cs
+++ Assets/Scripts/VI/UI_Button.cs
@@ -2,58 +2,132 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_Button : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler
 {
+
+    public enum PanelType {
+        MainMenu,
+        Information,
+        Setting
+    }
     public enum ButtonType {
+        None,
         Start,
         Option,
         Exit,
         Play,
-        Apply
+        Apply,
+        Guide,
+        Rule,
+        Item,
+        Event,
+        Card,
+        Resume,
+        Option_Ingame,
+        Researt,
+        Exit_Ingame
     }
+    public PanelType panelType;
+    public ButtonType buttonType;
 
-    public ButtonType type;
+    //MainMenu
     GameObject animObj;
 
+    //InformationPanel
+    Text informationText;
+    Image currentImage;
+    Color informatinoTextColor;
+
     void Awake() {
-        animObj = transform.Find("HL").gameObject;
-        animObj.SetActive(false);
+        switch (panelType) {
+            case PanelType.MainMenu:
+                animObj = transform.Find("HL").gameObject;
+                animObj.SetActive(false);
+                break;
+            case PanelType.Information:
+                informationText = GetComponentInChildren<Text>();
+                informatinoTextColor = informationText.color;
+                currentImage = GetComponent<Image>();
+                InGameUIController.i.informationButtons.Add(currentImage);
+                break;
+        }
     }
+
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (!animObj.activeInHierarchy) {
-            animObj.SetActive(true);
+        switch (panelType)
+        {
+            case PanelType.MainMenu:
+                if (!animObj.activeInHierarchy)
+                {
+                    animObj.SetActive(true);
+                }
+                break;
+            case PanelType.Information:
+                informationText.color = Color.white;
+                break;
         }
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (animObj.activeInHierarchy)
+        switch (panelType)
         {
-            animObj.SetActive(false);
+            case PanelType.MainMenu:
+                if (animObj.activeInHierarchy)
+                {
+                    animObj.SetActive(false);
+                }
+                break;
+            case PanelType.Information:
+                informationText.color = informatinoTextColor;
+
+                break;
         }
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        animObj.SetActive(false);
-        switch (type) {
-            case ButtonType.Start:
-                MainMenu.i.PressStart();
+        switch (panelType)
+        {
+            case PanelType.MainMenu:
+                if (animObj != null)
+                {
+                    animObj.SetActive(false);
+                }
+                switch (buttonType)
+                {
+                    case ButtonType.Start:
+                        MainMenu.i.PressStart();
+                        break;
+                    case ButtonType.Option:
+                        MainMenu.i.PressOption(true);
+                        break;
+                    case ButtonType.Exit:
+                        SceneController.ExitGame();
+                        break;
+                    case ButtonType.Play:
+                        SceneController.JumpScene(1);
+                        break;
+                    case ButtonType.Apply:
+                        MainMenu.i.PressOption(false);
+                        break;
+                }
                 break;
-            case ButtonType.Option:
-                MainMenu.i.PressOption(true);
+            case PanelType.Information:
+                InGameUIController.i.InformationButtonSelectd(currentImage);
                 break;
-            case ButtonType.Exit:
-                SceneController.ExitGame();
+            case PanelType.Setting:
+                switch (buttonType) {
+                    case ButtonType.Researt:
+                        SceneController.JumpScene(1);
+                        break;
+                    case ButtonType.Exit_Ingame:
+                        SceneController.ExitGame();
+                        break;
+                }
                 break;
-            case ButtonType.Play:
-                SceneController.JumpScene(1);
-                break;
-            case ButtonType.Apply:
-                MainMenu.i.PressOption(false);
-                break;
-
         }
     }
 
